7042de6f945bce9467bdcf590959f0e7c95ee710
spikeekips <spikeekips@4e5b0ac6-a417-8925-53e4-a6cf5892c026>
2010-12-17 05:16:37 +0000

    fixed bugs.

--- apps/webapp/master/context_processors.py
+++ apps/webapp/master/context_processors.py
@@ -4,11 +4,17 @@ from IO.project import models as models_project
 from IO.dialog import models as models_dialog
 
 def projects (request, ) :
+    if not request.user.is_authenticated() :
+        return dict()
+
     return dict(
         projects=models_project.Item.objects.all(),
     )
 
 def profile_labels (request, ) :
+    if not request.user.is_authenticated() :
+        return dict()
+
     return dict(
         profile_labels=models_dialog.Label.objects.filter(
                 profilemessage__profile=request.user.profile,
